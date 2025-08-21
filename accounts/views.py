a7d7fc75093813bd08244d83c9aed40c9fbecb48
sorowar jahan <sorowar230@gmail.com>
2025-08-20 23:56:17 -0400

    update files

--- accounts/views.py
+++ accounts/views.py
@@ -85,4 +85,6 @@ def activate(request, uidb64, token):
         messages.error(request, 'Invalid activation link.')
         return redirect('register')
 
-
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- shopnifty/settings.py
+++ shopnifty/settings.py
@@ -147,5 +147,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sorowarinvest@gmail.com'
-EMAIL_HOST_PASSWORD = 'ywnr ponq nkla exbs'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
