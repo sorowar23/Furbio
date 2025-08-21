704069993ce9988c7c0f36f92ae7e106c9fbcc25
sorowar jahan <sorowar230@gmail.com>
2025-08-20 23:21:26 -0400

    updated files for accounts module

--- shopnifty/settings.py
+++ shopnifty/settings.py
@@ -135,3 +135,17 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+#SMTP Configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sorowarinvest@gmail.com'
+EMAIL_HOST_PASSWORD = 'ywnr ponq nkla exbs'
+EMAIL_USE_TLS = True
